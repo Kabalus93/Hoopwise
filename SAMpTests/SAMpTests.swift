7430e4be305e888f1595a5de31a2b4ba3a4aac05
Luther Mutombo <luthermutombo@icloud.com>
2025-12-11 02:48:54 +0800

    Initial Commit

---
+++ SAMpTests/SAMpTests.swift
@@ -0,0 +1,16 @@
+//
+//  SAMpTests.swift
+//  SAMpTests
+//
+//  Created by Luther Mutombo on 12/11/25.
+//
+
+import Testing
+
+struct SAMpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
